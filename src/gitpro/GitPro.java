f696fef2efe0940c9597514058f863f16c8ba73a
Your Name <krasava1081@gmail.com>
2021-09-16 14:52:53 +0300

    third2

--- src/gitpro/GitPro.java
+++ src/gitpro/GitPro.java
@@ -16,6 +16,25 @@ public class GitPro {
      */
     public static void main(String[] args) {
         System.out.println("Первая версия проекта");
+        System.out.println("Вторая версия проекта");
+        /** Shift+f11
+        *Заходим в папку с проектом (на компьютере должен быть установлен гит)
+        *В папке открыть git bush (пкм)
+        *1. git init
+        *2. touch .gitignore
+        *3. Внутри гитигнор прописать:/build
+                                      /dist
+                                      /nbproject
+                                      build.xml
+        *4. Постоянно проверять git status, должно быть красным
+        *5. git add . 
+        *6. git commit -m""
+        *7. создать репозиторий в гит хаб
+        *8. скопировать git remote add origin... и вставить в гит баш
+        *9. git push origin master *Репозиторий готов и направлен в гитхаб*
+        */
+        
     }
+  
     
 }
